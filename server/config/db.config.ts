68a170b52c4f7518f179d05886265e5fb56de6e7
RushikeshSarap <rushikesh.jk9891@gmail.com>
2026-02-05 09:39:12 +0530

    1

--- server/config/db.config.ts
+++ server/config/db.config.ts
@@ -5,9 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: { rejectUnauthorized: false }
 });
 
 export const connectDB = async () => {
@@ -20,6 +18,4 @@ export const connectDB = async () => {
   }
 };
 
-export default {
-  query: (text: string, params?: any[]) => pool.query(text, params)
-};
+export default pool;
